1cad87803c949c191ed1edc941d7bfef74d50ff1
adogbeji <baka_1993@yahoo.co.uk>
2024-11-16 11:17:26 +0000

    Continue workign on light text theme

--- lib/utils/theme/custom_themes/text_theme.dart
+++ lib/utils/theme/custom_themes/text_theme.dart
@@ -4,7 +4,48 @@ class TTextTheme {
   TTextTheme._();
 
   static TextTheme lightTextTheme = TextTheme(
-    headlineLarge: TextStyle(),
+    headlineLarge: const TextStyle().copyWith(
+      fontSize: 32,
+      color: Colors.black,
+      fontWeight: FontWeight.bold,
+    ),
+    headlineMedium: const TextStyle().copyWith(
+      fontSize: 24,
+      color: Colors.black,
+      fontWeight: FontWeight.w600,
+    ),
+    headlineSmall: const TextStyle().copyWith(
+      fontSize: 18,
+      color: Colors.black,
+      fontWeight: FontWeight.w600,
+    ),
+
+    titleLarge: const TextStyle().copyWith(
+      fontSize: 16,
+      color: Colors.black,
+      fontWeight: FontWeight.w600,
+    ),
+    titleMedium: const TextStyle().copyWith(
+      fontSize: 16,
+      color: Colors.black,
+      fontWeight: FontWeight.w500,
+    ),
+    titleSmall: const TextStyle().copyWith(
+      fontSize: 16,
+      color: Colors.black,
+      fontWeight: FontWeight.w400,
+    ),
   );
-  static TextTheme darkTextTheme = TextTheme();
+  static TextTheme darkTextTheme = TextTheme(
+    headlineLarge: TextStyle().copyWith(
+      fontSize: 32,
+      color: Colors.black,
+      fontWeight: FontWeight.bold,
+    ),
+    headlineMedium: const TextStyle().copyWith(
+      fontSize: 24,
+      color: Colors.white,
+      fontWeight: FontWeight.w600,
+    ),
+  );
 }
