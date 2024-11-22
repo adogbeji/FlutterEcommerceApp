64a6877d4a26a914f51292021d7906a1ca92ce17
adogbeji <baka_1993@yahoo.co.uk>
2024-11-22 11:19:07 +0000

    Start creating app bar theme

---
+++ lib/utils/theme/custom_themes/appbar_theme.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class TAppBarTheme {
+
+  TAppBarTheme._();  // To avoid creating instances anywhere else
+
+  // Light Theme
+  static const lightAppBarTheme = AppBarTheme();
+
+  // Dark Theme
+  static const darkAppBarTheme = AppBarTheme();
+}
