85924471d38dd877756228bc3dbaa5f5604611e0
adogbeji <baka_1993@yahoo.co.uk>
2024-12-07 20:08:33 +0000

    Configure OutlinedButton theme for both light and dark modes

--- lib/utils/theme/custom_themes/chip_theme.dart
+++ lib/utils/theme/custom_themes/chip_theme.dart
@@ -13,11 +13,12 @@ class TChipTheme {
     checkmarkColor: Colors.white,
   );
 
-  static ChipThemeData darkChipTheme = ChipThemeData(
+  // Dark Theme
+  static ChipThemeData darkChipTheme = const ChipThemeData(
     disabledColor: Colors.grey,
-    labelStyle: const TextStyle(color: Colors.white),
+    labelStyle: TextStyle(color: Colors.white),
     selectedColor: Colors.blue,
-    padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
+    padding: EdgeInsets.symmetric(horizontal: 12, vertical: 12),
     checkmarkColor: Colors.white,
   );
 }
