e1cb696fa248d0de678bdaac26cb4e48236f3b1e
adogbeji <baka_1993@yahoo.co.uk>
2024-12-09 20:44:06 +0000

    Start defining class for TextFormField theme

---
+++ lib/utils/theme/custom_themes/text_form_field_theme.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class TTextFormFieldTheme {
+
+  TTextFormFieldTheme._();  // To avoid creating instances anywhere else
+
+  // Light Theme
+  static InputDecorationTheme lightInputDecorationTheme = const InputDecorationTheme();
+
+  // Dark Theme
+  static InputDecorationTheme darkInputDecorationTheme = const InputDecorationTheme();
+}
