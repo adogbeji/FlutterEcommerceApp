03ccc4b8bc54ade9a506e23ba0dadbf795350463
adogbeji <baka_1993@yahoo.co.uk>
2024-12-10 20:22:22 +0000

    Start configuring TextFormField theme for light mode

--- lib/utils/theme/custom_themes/text_form_field_theme.dart
+++ lib/utils/theme/custom_themes/text_form_field_theme.dart
@@ -5,7 +5,19 @@ class TTextFormFieldTheme {
   TTextFormFieldTheme._();  // To avoid creating instances anywhere else
 
   // Light Theme
-  static InputDecorationTheme lightInputDecorationTheme = const InputDecorationTheme();
+  static InputDecorationTheme lightInputDecorationTheme = InputDecorationTheme(
+    errorMaxLines: 3,
+    prefixIconColor: Colors.grey,
+    suffixIconColor: Colors.grey,
+    labelStyle: const TextStyle().copyWith(fontSize: 14, color: Colors.black),
+    hintStyle: const TextStyle().copyWith(fontSize: 14, color: Colors.black),
+    errorStyle: const TextStyle().copyWith(fontStyle: FontStyle.normal),
+    floatingLabelStyle: const TextStyle().copyWith(color: Colors.black.withOpacity(0.8)),
+    border: OutlineInputBorder().copyWith(
+      borderRadius: BorderRadius.circular(14),
+      borderSide: const BorderSide(width: 1, color: Colors.grey),
+    ),
+  );
 
   // Dark Theme
   static InputDecorationTheme darkInputDecorationTheme = const InputDecorationTheme();
