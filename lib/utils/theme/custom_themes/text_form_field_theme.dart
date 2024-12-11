520f4e139bb2dd7fe7d076afd43b57a7140031b9
adogbeji <baka_1993@yahoo.co.uk>
2024-12-11 20:09:21 +0000

    Add more configuration to TextFormField theme for light mode

--- lib/utils/theme/custom_themes/text_form_field_theme.dart
+++ lib/utils/theme/custom_themes/text_form_field_theme.dart
@@ -13,9 +13,17 @@ class TTextFormFieldTheme {
     hintStyle: const TextStyle().copyWith(fontSize: 14, color: Colors.black),
     errorStyle: const TextStyle().copyWith(fontStyle: FontStyle.normal),
     floatingLabelStyle: const TextStyle().copyWith(color: Colors.black.withOpacity(0.8)),
-    border: OutlineInputBorder().copyWith(
+    border: const OutlineInputBorder().copyWith(
       borderRadius: BorderRadius.circular(14),
       borderSide: const BorderSide(width: 1, color: Colors.grey),
+    ),
+    enabledBorder: const OutlineInputBorder().copyWith(
+      borderRadius: BorderRadius.circular(14),
+      borderSide: const BorderSide(width: 1, color: Colors.grey),
+    ),
+    focusedBorder: OutlineInputBorder().copyWith(
+      borderRadius: BorderRadius.circular(14),
+      borderSide: const BorderSide(width: 1, color: Colors.black12),
     ),
   );
 
