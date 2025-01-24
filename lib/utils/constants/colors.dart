d3be996fc1a2dce2c2bd3814fce63225021fa0e0
adogbeji <baka_1993@yahoo.co.uk>
2025-01-24 14:44:29 +0000

    Start definint colour constants

--- lib/utils/constants/colors.dart
+++ lib/utils/constants/colors.dart
@@ -3,6 +3,14 @@ import 'package:flutter/material.dart';
 class TColors {
   TColors._();
 
+  // App Basic Colours
   static const Color primary = Color(0xFF4B68FF);
   static const Color secondary = Color(0xFFFFE24B);
+  static const Color accent = Color(0xFFb0c7FF);
+
+
+  // Text Colours
+  static const Color textPrimary = Color(0xFF333333);
+  static const Color textSecondary = Color(0xFF6C757D);
+  static const Color textWhite = Colors.white;
 }
