a3381e134883d0c7eb94983876eba46befe2d248
adogbeji <baka_1993@yahoo.co.uk>
2025-02-06 14:44:13 +0000

    Define more colour constants

--- lib/utils/constants/colors.dart
+++ lib/utils/constants/colors.dart
@@ -13,4 +13,18 @@ class TColors {
   static const Color textPrimary = Color(0xFF333333);
   static const Color textSecondary = Color(0xFF6C757D);
   static const Color textWhite = Colors.white;
+
+  // Background Colours
+  static const Color light = Color(0xFFF6F6F6);
+  static const Color dark = Color(0xFF272727);
+  static const Color primaryBackground = Color(0xFFF3F5FF);
+
+  // Background Container Colours
+  static const Color lightContainer = Color(0xFFF6F6F6);
+  static Color darkContainer = Colors.white.withOpacity(0.1);
+
+  // Button Colours
+  static const Color buttonPrimary = Color(0xFF4b68FF);
+  static const Color buttonSecondary = Color(0xFF6C757D);
+  static const Color buttonDisabled = Color(0xFFC4C4C4);
 }
