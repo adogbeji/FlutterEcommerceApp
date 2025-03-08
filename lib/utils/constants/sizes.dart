415dadcd87572bc0649935007ca1168214ad4a6d
adogbeji <baka_1993@yahoo.co.uk>
2025-03-08 09:21:34 +0000

    Add more size constants

--- lib/utils/constants/sizes.dart
+++ lib/utils/constants/sizes.dart
@@ -5,4 +5,23 @@ class TSizes {
   static const double md = 16.0;
   static const double lg = 24.0;
   static const double xl = 32.0;
+
+
+  // Icon Sizes
+  static const double iconXs = 12.0;
+  static const double iconSm = 16.0;
+  static const double iconMd = 24.0;
+  static const double iconLg = 32.0;
+
+
+  // Font Sizes
+  static const double fontSizeSm = 14.0;
+  static const double fontSizeMd = 16.0;
+  static const double fontSizeLg = 18.0;
+
+
+  // Button Sizes
+  static const double buttonHeight = 18.0;
+  static const double buttoniRadius = 12.0;
+  static const double buttonWidth = 120.0;
 }
