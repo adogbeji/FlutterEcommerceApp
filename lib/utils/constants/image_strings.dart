ce53706756d72321e3e5b1d139c4d75b29a9d7be
adogbeji <baka_1993@yahoo.co.uk>
2025-04-26 09:34:08 +0100

    Add placeholder image strings in TImages class

--- lib/utils/constants/image_strings.dart
+++ lib/utils/constants/image_strings.dart
@@ -1,1 +1,11 @@
-class TImages {}
+class TImages {
+
+  // App Logos
+  static const String darkAppLogo = 'assets/images/logo-1.png';
+  static const String lightAppLogo = 'assets/images/logo-2.png';
+
+
+  // Social Logos
+  static const String google = 'assets/logos/google.png';
+  static const String facebook = 'assets/logos/facebook.png';
+}
